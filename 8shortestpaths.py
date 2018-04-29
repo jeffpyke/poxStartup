f5a4f8b1af1869020fbd9ac13b32051877f3d25e
Jeff <pyke@stanford.edu>
2018-04-29 13:41:33 -0700

    8shortest paths

--- 8shortestpaths.py
+++ 8shortestpaths.py
@@ -3,6 +3,9 @@ from itertools import islice
 from collections import Counter
 import itertools
 from tqdm import tqdm
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 def k_shortest_paths(G, source, target, k, weight=None):
     return list(islice(nx.shortest_simple_paths(G, source, target, weight=weight), k))
@@ -21,7 +24,7 @@ def group(lst, n):
 # each rack has k-r ports available for servers
 
 # Degree of each node, Number of Nodes
-G = nx.random_regular_graph(4, 100)
+G = nx.random_regular_graph(12, 25)
 
 # id of link is (a,b), where a and b are routers
 links = Counter()
@@ -34,4 +37,11 @@ for source in tqdm(G.nodes()):
       up += [grp for grp in group(path,2)]
     links.update(up)
 
-print links
+ysorted = links.most_common()
+ysorted.reverse()
+y = [val[1] for val in ysorted]
+x = np.arange(1,len(links.most_common())+1)
+plt.plot(x, y)
+plt.xlabel('Rank of link')
+plt.ylabel('# distinct paths through link')
+plt.show()
